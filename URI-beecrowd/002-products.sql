d6f72324b631e934a42d6841f3a573a9ae743570
Everton Santos <everton_projeto@outlook.com>
2024-10-23 18:48:57 -0300

    atualizando tabela products

--- URI-beecrowd/002-products.sql
+++ URI-beecrowd/002-products.sql
@@ -14,5 +14,6 @@ VALUES
 	(1,	'Two-door wardrobe', 100, 80),
 	(2,	'Dining table',	1000, 560),
 	(3,	'Towel holder',	10000, 5.50),
-	(4,	'Computer desk', 350, 100);
-
+	(4,	'Computer desk', 350, 100),
+	(5,	'Chair', 3000, 210.64),
+	(6,	'Single bed', 750, 99);
