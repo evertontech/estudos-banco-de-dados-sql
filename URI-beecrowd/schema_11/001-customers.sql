87f7ab061597df2a366301bba44bed0e619895f1
Everton Santos <everton_projeto@outlook.com>
2024-10-29 17:06:19 -0300

    schema_11

---
+++ URI-beecrowd/schema_11/001-customers.sql
@@ -0,0 +1,17 @@
+CREATE TABLE customers(
+	"id"		NUMERIC	PRIMARY KEY,
+	"name"		VARCHAR(40),
+	"street"	VARCHAR(40),
+	"city"		VARCHAR(40)
+);
+
+-- INSERÇÃO DE DADOS NA TABELA CUSTOMERS 2615
+
+INSERT INTO customers("id", "name", "street", "city")
+VALUES
+	(1, 'Giovanna Goncalves Oliveira', 'Rua Mato Grosso', 'Canoas'),
+	(2, 'Kauã Azevedo Ribeiro', 'Travessa Ibiá', 'Uberlândia'),
+	(3, 'Rebeca Barbosa Santos', 'Rua Observatório Meteorológico', 'Salvador'),
+	(4, 'Sarah Carvalho Correia', 'Rua Antônio Carlos da Silva', 'Uberlândia'),
+	(5, 'João Almeida Lima', 'Rua Rio Taiuva', 'Ponta Grossa'),
+	(6, 'Diogo Melo Dias', 'Rua Duzentos e Cinqüenta', 'Várzea Grande');
