3dce3977126fc8b6e1670d6d39f8994bb389da9a
Everton Santos <everton_projeto@outlook.com>
2024-10-30 18:47:56 -0300

    schema_15

--- URI-beecrowd/schema_15/002-orders.sql
+++ URI-beecrowd/schema_15/002-orders.sql
@@ -1,15 +1,15 @@
 -- CRIAÇÃO DA TABELA ORDERS 2620
 
-CREATE TABLE orders(
-	"id"			NUMERIC PRIMARY KEY,
-	orders_date		DATE,
-	id_customers	NUMERIC,
-	FOREIGN KEY (id_customers) REFERENCES customers("id")
+CREATE TABLE "orders"(
+	"id"				NUMERIC PRIMARY KEY,
+	"orders_date"		DATE,
+	"id_customers"		NUMERIC,
+	FOREIGN KEY ("id_customers") REFERENCES "customers"("id")
 );
 
 -- INSERÇÃO DE DADOS NA TABELA ORDERS 2620
 
-INSERT INTO orders("id", orders_date, id_customers)
+INSERT INTO "orders"("id", "orders_date", "id_customers")
 VALUES
 	(1,	'2016-05-13', 3),
 	(2,	'2016-01-12', 2),
