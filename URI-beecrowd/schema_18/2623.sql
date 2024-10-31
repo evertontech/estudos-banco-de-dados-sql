6d8cf32bd58223f7c8c49cc59a82acf981598459
Everton Santos <everton_projeto@outlook.com>
2024-10-31 18:36:30 -0300

    uri 2623

---
+++ URI-beecrowd/schema_18/2623.sql
@@ -0,0 +1,7 @@
+SELECT prod."name", cat."name"
+FROM products prod
+JOIN categories cat ON cat."id" = prod."id_categories"
+WHERE
+	prod."amount" > 100
+	AND prod."id_categories" IN (1, 2, 3, 6, 9);
+	
